0a6bb40c198f3564d1084bddfacacee1e6432582
Agustin Allende <agus.allende996@gmail.com>
2025-12-31 21:35:50 -0300

    Fix de errores

--- FutbolTurnos/src/components/QRPage.jsx
+++ FutbolTurnos/src/components/QRPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import QRCode from 'qrcode.react'
 import "bootstrap/dist/css/bootstrap.min.css"; 
 import { useNavigate, useParams } from "react-router-dom";
 import {
@@ -18,16 +19,20 @@ const QRPage = () => {
     navigate("/turnos");
   };
   const { id } = useParams(); 
-  const qrUrl = `http://localhost:59470/api/Qr/Generar/${id}`;
+  const datosQR = JSON.stringify({
+    reservaId:id,
+    mensaje:"Reserva confirmada"
+  })
+  // const qrUrl = `http://localhost:59470/api/Qr/Generar/${id}`;
 
-  const handleDownload = () => {
-    const link = document.createElement("a")
-    link.href = qrUrl
-    link.download = `reserva_${id}.png`
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
-  }
+  // const handleDownload = () => {
+  //   const link = document.createElement("a")
+  //   link.href = qrUrl
+  //   link.download = `reserva_${id}.png`
+  //   document.body.appendChild(link)
+  //   link.click()
+  //   document.body.removeChild(link)
+  // }
 
   
 
@@ -40,11 +45,14 @@ const QRPage = () => {
       outline
       style={{ width: "18rem", margin: "40px auto" }}
     >
-      <img
+      <div style={{ background: "white", padding: "10px" }}>
+  <QRCode value={datosQR} size={200} />
+</div>
+      {/* <img
         alt="QR Code"
         src={`http://localhost:59470/api/Qr/Generar/${id}`}
         style={{ width: "100%", padding: "10px" }}
-      />
+      /> */}
       <CardBody>
         <CardTitle tag="h5">¡Reserva confirmada!</CardTitle>
         <CardSubtitle className="mb-2 text-muted" tag="h6">
@@ -54,7 +62,7 @@ const QRPage = () => {
           <p>Gracias por tu reserva, nos vemos en la cancha ⚽</p>
         </CardText>
         <Button onClick={handleInicio} color="light">Volver al inicio</Button>
-        <Button color="light" onClick={handleDownload} >Descargar QR</Button>
+        {/* <Button color="light" onClick={handleDownload} >Descargar QR</Button> */}
       </CardBody>
     </Card>
   );
